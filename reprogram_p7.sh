89868b54dfde2a726272e4650a9868b784eec717
Andrew Ong <andrew@happy.ai>
2021-07-12 22:38:04 -0700

    add script to reprogram P7

---
+++ reprogram_p7.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+APP_BIN="C:\hh\dialog_14683_scratch\projects\dk_apps\templates\freertos_retarget\Happy_P7_QSPI_Release\freertos_retarget.binh"
+DIALOG_WORKSPACE="/c/hh/dialog_14683_scratch"
+BL_BIN="C:\hh\dialog_14683_scratch\sdk\bsp\system\loaders\ble_suota_loader\DA14683-00-Release_QSPI\ble_suota_loader.bin"
+BL_UTILS_PATH="${DIALOG_WORKSPACE}/utilities/scripts/qspi"
+HPY_UTILS_PATH="${DIALOG_WORKSPACE}/utilities/scripts/hpy/v11"
+# Example command
+#./program_qspi_jtag.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" \
+# "C:\Dialog_Workspaces\Dialog_FreeRTOS_01\sdk\bsp\system\loaders\ble_suota_loader\DA14683-00-Release_QSPI\ble_suota_loader.bin"
+pushd "${BL_UTILS_PATH}"
+./program_qspi_jtag.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" "${BL_BIN}"
+popd
+# Example command
+#./initial_flash.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" \
+# "C:\Dialog_Workspaces\Dialog_FreeRTOS_01\projects\dk_apps\templates\freertos_retarget\Happy_P6_QSPI_Debug\freertos_retarget.bin"
+pushd "${HPY_UTILS_PATH}"
+./initial_flash.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" "${APP_BIN}"
+popd
