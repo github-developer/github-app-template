bd74dee9215320fbc5b00fe1c7b3d912101820a6
Andrew Ong <andrew@happy.ai>
2021-07-16 20:16:19 -0700

    gitignore and remove an error

--- reprogram_p7.sh
+++ reprogram_p7.sh
@@ -11,7 +11,7 @@ HPY_UTILS_PATH="${DIALOG_WORKSPACE}/utilities/scripts/hpy/v11"
 #./program_qspi_jtag.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" \
 # "C:\Dialog_Workspaces\Dialog_FreeRTOS_01\sdk\bsp\system\loaders\ble_suota_loader\DA14683-00-Release_QSPI\ble_suota_loader.bin"
 pushd "${BL_UTILS_PATH}"
-./program_qspi_jtag.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" "${BL_BIN}s" || exit 1
+./program_qspi_jtag.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" "${BL_BIN}" || exit 1
 popd
 # Example command
 #./initial_flash.bat --jlink_path "c:\Program Files (x86)\SEGGER\JLink_V612i" \
