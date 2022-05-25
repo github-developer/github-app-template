702e31d604495f76dfd09bdda488618bcd832584
Andrés Felipe Barco Santa <andres@engflow.com>
2022-05-25 16:17:15 -0500

    Removed test

--- template_server.rb
+++ template_server.rb
@@ -25,9 +25,7 @@ set :bind, '0.0.0.0'
 # Of course, not all apps need to receive and process events!
 # Feel free to rip out the event handling code if you don't need it.
 #
-# Have fun! I will
-#
-# other comments
+
 
 class GHAapp < Sinatra::Application
 
